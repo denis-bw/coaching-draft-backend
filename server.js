4ba2626fbf3551eb9cdd74f14f106fb58c7dcde1
Denis Belousov <109822229+denis-bw@users.noreply.github.com>
2025-02-07 15:43:06 +0200

    bug fix cros

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],  
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'],  
 };
 
 app.use(cors(corsOptions));  
